8359b5aae47c0d537a1499efd7e078803189bfef
utku <bayraktarogluutku@gmail.com>
2022-12-13 17:41:27 +0300

    Basic CSS finish

--- hipotenus.js
+++ hipotenus.js
@@ -1,20 +1,28 @@
 $(document).ready(function () {
 
     const numList = []
-
+    const column = document.getElementById("column")
 
 arrPush()
     function arrPush() {
 
-        var pointer = 0
+        var num = 0
         for (let index = 0; index < 49; index++) {
-            pointer++
-            numList.push(pointer)
+            num++
+            numList.push(num)
+            
+            var span = document.createElement("span")
+            span.className = "ball pt-2"
+            span.innerHTML += `${num}`
+
+
+            column.appendChild(span)
+
         }
     }
 
 
-    
+
 
     
 
