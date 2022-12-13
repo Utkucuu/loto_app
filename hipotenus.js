b8d33ab50cf03a463a52d88db7662e7d519db1d0
utku <bayraktarogluutku@gmail.com>
2022-12-13 22:44:25 +0300

    mid panel finish

--- hipotenus.js
+++ hipotenus.js
@@ -3,53 +3,83 @@ $(document).ready(function () {
     const numList = []
     const column = document.getElementById("column")
 
-arrPush()
+    arrPush()
     function arrPush() {
 
         var num = 0
-        
-       var timeControl = setInterval(function () { 
-           num++
+
+        var timeControl = setInterval(function () {
+            num++
             numList.push(num)
-            
+
             var span = document.createElement("span")
             span.className = "ball pt-2 "
             span.innerHTML += `${num}`
 
-         
-                column.appendChild(span)
-                $(span).fadeIn("");
+            column.appendChild(span)
+            $(span).fadeIn("");
 
-                if(num == 49){
-                    clearInterval(timeControl)
-                }
-        },100)
-        
-         
+            if (num == 49) {
+                clearInterval(timeControl)
+            }
+        }, 100)
+
+
     }
 
     const selectNum = document.getElementById("selectNum")
     const leftPanel = document.getElementById("leftPanel")
-    
-    console.log(selectNum)
 
-    $("#addBtn").click(function () { 
-        
+
+
+    var c = 0
+
+    const selecList = []
+
+
+    $("#addBtn").click(function () {
+
+
+
         var numDiv = document.createElement("div")
-        
+
         numDiv.innerHTML = selectNum.value
+        numDiv.className = "letfNum"
 
-        leftPanel.appendChild(numDiv)
+        if (0 < selectNum.value && selectNum.value < 50) {
 
-     
+            if (!selecList.includes(selectNum.value)) {
+
+
+                c++
+                leftPanel.appendChild(numDiv)
+
+                selecList.push(selectNum.value)
+
+                selectNum.value = ""
+
+                if (c == 6) {
+                    $("#addBtn").attr('disabled', 'disabled');
+                    // $("#start").css('display','block' );
+                    // $("#start").css('align-items', 'center' );
+                     document.getElementById("midPanel").innerHTML +=` <div class="text-center" ><button id="start" class="btn btn-danger mt-2">Başla</button></div>`
+
+
+                }
+            }
+        }
+
+        console.log()
+        
+
     });
-    
-    
 
 
 
-    
 
-  
+
+
+
+
 
 });
