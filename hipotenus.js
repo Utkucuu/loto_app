007c0fe44115053386429a2ac5a601af4252743e
utku <bayraktarogluutku@gmail.com>
2022-12-13 17:07:48 +0300

    counting compalted

--- hipotenus.js
+++ hipotenus.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
 
+    const numList = []
 
+
+arrPush()
+    function arrPush() {
+
+        var pointer = 0
+        for (let index = 0; index < 49; index++) {
+            pointer++
+            numList.push(pointer)
+        }
+    }
+
+
+    
+
+    
+
+    console.log(numList)
 
 });
